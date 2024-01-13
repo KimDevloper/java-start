89ad90e1ae4394d4c243b9c23be807834596447a
김대원 <a62387272@gmail.com>
2024-01-13 17:26:00 +0900

    [ADD] Hello Java

--- src/CommentJava.java
+++ src/CommentJava.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class CommentJava {
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java"); // 주석 테스트
+    }
 }
