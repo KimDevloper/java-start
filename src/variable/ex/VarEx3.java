215aebdecad4445f003f91279610f63f18cdb1d0
김대원 <a62387272@gmail.com>
2024-01-13 17:26:35 +0900

    [ADD] 변수 예제 풀이

--- src/variable/ex/VarEx3.java
+++ src/variable/ex/VarEx3.java
@@ -1,2 +1,10 @@
-package variable.ex;public class VarEx3 {
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long s = 10000000000L;
+        System.out.println(s);
+        boolean a = true;
+        System.out.println(a);
+    }
 }
