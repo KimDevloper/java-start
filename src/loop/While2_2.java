fc842c6a4905846bc421120703e8609a891dfab2
김대원 <a62387272@gmail.com>
2024-01-22 17:17:31 +0900

    [ADD] Break문 학습

---
+++ src/loop/While2_2.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class While2_2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 100;
+        sum = sum + i;
+        System.out.println("i =" + 1 + "sum : "+ sum);
+        sum = sum + i;
+        System.out.println("i =" + 1 + "sum : "+ sum);
+        sum = sum + i;
+        System.out.println("i =" + 1 + "sum : "+ sum);
+    }
+}
