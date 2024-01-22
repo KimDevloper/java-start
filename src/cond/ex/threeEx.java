9b990465ce5448860444026c6f5f2fd689e4f6eb
김대원 <a62387272@gmail.com>
2024-01-22 17:17:06 +0900

    [ADD] 삼항문 학습

---
+++ src/cond/ex/threeEx.java
@@ -0,0 +1,9 @@
+package cond.ex;
+
+public class threeEx {
+    public static void main(String[] args) {
+        int x = 2;
+        String res = (x % 2 == 0) ? "짝수" : "홀수" ;
+        System.out.println("결과 :" + res);
+    }
+}
